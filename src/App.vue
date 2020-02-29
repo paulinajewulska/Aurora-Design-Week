ba94540e3252e5e3ff536b94c497cc7714ea1ca2
paulinajewulska <paulina.jewulska@gmail.com>
2020-02-29 18:04:01 +0100

    Add normalize.scss

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <app-landing-page></app-landing-page>
+  </div>
 </template>
+
+<script>
+import LandingPage from "@/views/LandingPage.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppLandingPage: LandingPage
+  }
+};
+</script>
+
+<style>
+@import "./styles/_normalize.scss";
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="landing-page"></section>
+</template>
+
+<script>
+export default {
+  name: "LandingPage"
+};
+</script>
+
+<style lang="scss" scoped>
+.landing-page {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
