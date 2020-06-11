f0a007a55eaca6fcb8b5317a673806cd4e3f8ee3
paulinajewulska <paulina.jewulska@gmail.com>
2020-06-11 19:49:46 +0200

    Fix server set up

--- src/services/server.js
+++ src/services/server.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/../dist')))
+app.use('/', serveStatic(path.join(__dirname, '/../../dist')))
 
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/index.html'))
+    res.sendFile(path.join(__dirname, '/../../dist/index.html'))
 })
 
 const port = process.env.PORT || 8080
