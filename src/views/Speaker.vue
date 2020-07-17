32b8d6dcf53142deaa946c436ca6d79803cdcee1
paulinajewulska <paulina.jewulska@gmail.com>
2020-07-17 22:25:45 +0200

    Redesign speaker subpage

--- src/views/Speaker.vue
+++ src/views/Speaker.vue
@@ -3,51 +3,44 @@
     class="speaker"
     @mouseover="changeCursor({ color: 'black', hover: false })"
   >
-    <h2
-      class="speaker__title column is-12-mobile is-6-tablet is-6-desktop is-6-fullhd"
-    >
-      {{ $route.params.name }} {{ $route.params.surname }}
-    </h2>
-    <v-lazy-image
-      class="speaker__image is-12-mobile is-6-tablet is-6-desktop is-6-fullhd"
-      :src="getSpeaker($route.params.name, $route.params.surname).image"
-    />
-    <p
-      class="speaker__info column is-12-mobile is-6-tablet is-6-desktop is-6-fullhd"
-    >
-      Nam ut libero lectus. Nulla viverra leo diam, sit amet viverra sapien
-      mollis eget. Orci varius natoque penatibus et magnis dis parturient
-      montes, nascetur ridiculus mus. Fusce accumsan urna aliquam tellus
-      porttitor, ultricies tempus orci molestie. Morbi a porta ligula.
-      Vestibulum eget tortor eros. Cras ullamcorper in neque sit amet tempor.
-      Morbi ac consectetur felis. Vestibulum dictum, urna non lobortis aliquam,
-      lacus enim varius libero, ut ultricies sem augue id lectus. Sed commodo ac
-      purus vitae bibendum. Donec gravida, turpis sed commodo mattis, nibh est
-      venenatis nisl, non vehicula lectus libero in odio. Cras auctor faucibus
-      consectetur. Nam non tellus placerat, vulputate quam vel, vulputate erat.
-    </p>
-    <p
-      class="speaker__info column is-12-mobile is-6-tablet is-6-desktop is-6-fullhd"
-    >
-      Integer a elementum est. Ut molestie malesuada ante, eu rhoncus mi
-      venenatis quis. Duis quis odio sed ex venenatis mattis nec nec sem. Lorem
-      ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique cursus
-      leo. Fusce egestas aliquam justo nec dictum. Quisque metus est, dignissim
-      eget libero non, feugiat iaculis mauris. Quisque erat velit, semper vitae
-      euismod et, sagittis a velit. Proin nec orci sit amet diam sagittis
-      dignissim. Phasellus nec arcu nec lectus condimentum vestibulum. Aliquam
-      erat volutpat.
-    </p>
-    <router-link
-      class="speaker__button column is-12-mobile is-12-tablet is-12-desktop is-12-fullhd"
-      v-if="!isMenuOpen"
-      @mousemove.native="changeCursor({ color: 'red', hover: true })"
-      @mouseleave.native="changeCursor({ color: 'black', hover: false })"
-      :to="{
-        name: 'speakers'
-      }"
-      >GO BACK TO SPEAKERS</router-link
-    >
+    <div class="speaker__image-wrapper">
+      <v-lazy-image
+        class="speaker__image"
+        :src="getSpeaker($route.params.name, $route.params.surname).image"
+      />
+      <h2
+        class="speaker__title column is-12-mobile is-6-tablet is-6-desktop is-6-fullhd"
+      >
+        <span class="speaker__title__name">{{ $route.params.name }}</span>
+        <span class="speaker__title__surname">{{ $route.params.surname }}</span>
+      </h2>
+    </div>
+    <div class="speaker__below-image">
+      <div class="speaker__line"></div>
+      <p class="speaker__info">
+        Nam ut libero lectus. Nulla viverra leo diam, sit amet viverra sapien
+        mollis eget. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Fusce accumsan urna aliquam tellus
+        porttitor, ultricies tempus orci molestie. Morbi a porta ligula.
+        Vestibulum eget tortor eros. Cras ullamcorper in neque sit amet tempor.
+        Morbi ac consectetur felis. Vestibulum dictum, urna non lobortis
+        aliquam, lacus enim varius libero, ut ultricies sem augue id lectus. Sed
+        commodo ac purus vitae bibendum. Donec gravida, turpis sed commodo
+        mattis, nibh est venenatis nisl, non vehicula lectus libero in odio.
+        Cras auctor faucibus consectetur. Nam non tellus placerat, vulputate
+        quam vel, vulputate erat.
+      </p>
+      <router-link
+        class="speaker__button column is-12-mobile is-12-tablet is-12-desktop is-12-fullhd"
+        v-if="!isMenuOpen"
+        @mousemove.native="changeCursor({ color: 'red', hover: true })"
+        @mouseleave.native="changeCursor({ color: 'black', hover: false })"
+        :to="{
+          name: 'speakers'
+        }"
+        >Go back to the speakers</router-link
+      >
+    </div>
   </section>
 </template>
 
@@ -75,45 +68,128 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "../sass/main.scss";
+
 .speaker {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
   width: 100vw;
   min-height: 100vh;
 
+  &__image-wrapper {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: auto;
+    @media only screen and (min-width: $desktop) {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+    }
+  }
+
   &__image {
-    margin: 4rem auto;
-    @media only screen and (min-width: 769px) {
-      margin-left: 1rem;
-      width: calc(50% - 1rem);
+    display: block;
+    width: 65vw;
+    height: auto;
+    margin: 12.5vh auto 0;
+    @media only screen and (min-width: $tablet) {
+      width: 50%;
+      margin: 10vh 0 0 7.5vw;
+    }
+    @media only screen and (min-width: $desktop) {
+      position: absolute;
+      top: $margin-fullhd;
+      left: 10vw;
+      width: 30vw;
+      height: calc(100vh - 2rem);
+      margin: 0;
     }
   }
 
   &__title {
-    margin: auto;
+    position: absolute;
+    bottom: -4rem;
+    margin: 0;
     padding: 0;
-    font-size: 5rem;
+    text-align: center;
+    font-size: 3rem;
     text-transform: uppercase;
     line-height: 4rem;
-    word-break: break-word;
-  }
-  &__info {
-    margin-bottom: 4rem;
-    padding: 0;
-    @media only screen and (min-width: 769px) {
-      &:first-of-type {
-        padding-right: 1rem;
+    &__name,
+    &__surname {
+      display: block;
+    }
+    @media only screen and (min-width: $tablet) {
+      width: 50%;
+      bottom: calc(40% - 4rem);
+      left: 45vw;
+      text-align: left;
+      &__name {
+        margin-right: 1rem;
       }
-      &:last-of-type {
-        padding-left: 1rem;
+    }
+    @media only screen and (min-width: $desktop) {
+      top: 20vh;
+      font-size: 5rem;
+      &__name {
+        position: absolute;
+        top: 0;
+        width: auto;
+      }
+      &__surname {
+        position: absolute;
+        top: 4rem;
+        left: 15vw;
+        width: auto;
       }
     }
   }
+
+  &__below-image {
+    position: relative;
+    width: 100%;
+    height: auto;
+    margin: 6rem 0 2rem;
+    @media only screen and (min-width: $tablet) {
+      margin: 4rem 0 1rem;
+    }
+    @media only screen and (min-width: $desktop) {
+      position: absolute;
+      bottom: 1rem;
+      left: 45vw;
+      width: 45vw;
+      margin-bottom: 0;
+    }
+  }
+
+  &__line {
+    display: block;
+    width: 1px;
+    height: 7.5vh;
+    margin: 1rem auto;
+    background-color: $black;
+    @media only screen and (min-width: $tablet) {
+      display: none;
+    }
+  }
+
+  &__info {
+    width: 100%;
+    margin: 2rem 0;
+    padding: 0;
+    text-align: justify;
+    @media only screen and (min-width: $tablet) {
+      margin: 0 0 3rem;
+    }
+    @media only screen and (min-width: $desktop) {
+      min-height: 40vh;
+    }
+  }
+
   &__button {
+    width: fit-content;
     border: none;
     border-bottom: 1px solid $black;
-    width: fit-content;
     padding: 0;
     background-color: transparent;
     color: $black;
